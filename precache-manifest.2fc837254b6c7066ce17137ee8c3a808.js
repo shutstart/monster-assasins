da4f1d71571051c06e99fad026d67e9ed2daf19b
shutstart <chirag.189301201@muj.manipal.edu>
2019-12-16 20:40:10 +0530

    Updates

---
+++ precache-manifest.2fc837254b6c7066ce17137ee8c3a808.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8502f4f276f3d5763fd7798c3c4d9ebc",
+    "url": "/monster-assasins/index.html"
+  },
+  {
+    "revision": "2502a62f3ee01d054ded",
+    "url": "/monster-assasins/static/css/main.ab238b23.chunk.css"
+  },
+  {
+    "revision": "a4dff5b7c70ab47a72d5",
+    "url": "/monster-assasins/static/js/2.d612ab20.chunk.js"
+  },
+  {
+    "revision": "2502a62f3ee01d054ded",
+    "url": "/monster-assasins/static/js/main.7f9eab4a.chunk.js"
+  },
+  {
+    "revision": "b57e0f9867ee16862128",
+    "url": "/monster-assasins/static/js/runtime-main.d0ceab74.js"
+  }
+]);
